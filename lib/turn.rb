c010442497a85253541501a1801b1cfb4e0783fa
Alisher Zakir <a3zakir@gmail.com>
2019-04-21 23:36:29 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -24,7 +24,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   end
-  move(board, index) if valid_move?(index)
+  move(board, index) if valid_move?(board, index)
 end
 
 
