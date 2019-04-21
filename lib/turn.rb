e67d15880f857f3ec4627aca2bcea827fbe2650c
Alisher Zakir <a3zakir@gmail.com>
2019-04-21 23:13:36 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,3 +4,14 @@ def display_board(board)
   puts " #{board[3]} || #{board[4]} || #{board[5]} "
   puts "---------"
   puts " #{board[6]} || #{board[7]} || #{board[8]} "
+end
+
+def valid_move?(board, index)
+  return false if !index.between(0,8)
+  return !(board[index] == "X" || board[index] == "O")
+end
+
+def move(board, index, char="X")
+  board[index] = char
+  return board
+end
